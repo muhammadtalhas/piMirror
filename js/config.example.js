574bd91047f7912be92648502da799a5e3140308
Talha Muhammad <muhammadtalhas@yahoo.com>
2016-05-07 19:56:25 +1300

    adding default config file
    rename to config.js when needed

---
+++ js/config.example.js
@@ -0,0 +1,17 @@
+var config = {
+    weather: {
+        location: "LOCATION",//Can be zip code, address, street, just about anything you can google
+        apikey: "KEY_HERE"//forcast.io api key
+    },
+    location: {
+        location: "LOCATION",
+        googleKey : "GOOGLE_LOCATION_KEY"//Not needed currently
+    },
+    time : {
+        format: 12,//Default to 24 if not provided/invalid
+        seconds: false 
+    }
+    
+    
+    
+}
