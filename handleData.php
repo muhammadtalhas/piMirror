6cbb9170d8ab3a9e1c99068db7e50312cb0aeac9
Talha Muhammad <muhammadtalhas@yahoo.com>
2016-05-27 01:16:39 -0400

    Web layout of adding users
    User management will happen through another webportal

---
+++ handleData.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["first_name"]; ?><br>
+Your email address is: <?php echo $_POST["IMEI"]; ?>
+
+<?php print_r($_POST); ?>
+
+
+</body>
+</html>
--- node/userServer.js
+++ node/userServer.js
@@ -3,25 +3,42 @@ var app = express();
 var fs = require("fs");
 
 var currentUser = {
-    primaryKey : null
-    
+    primaryKey: null
+
 }
 
-app.get('/test/:imei', function (req, res) {
-  console.log("Phone data recieved! " + req.params.imei);
-  currentUser.primaryKey = req.params.imei;
+var loginMsg = {
+    loggedIn: "Data recieved!"
+}
+var logoutTimerId;
+
+app.get('/test/:imei', function(req, res) {
+    console.log("Phone data recieved! " + req.params.imei);
+    currentUser.primaryKey = req.params.imei;
+    fs.readFile(__dirname + "/" + "response.json", 'utf8', function(err, data) {
+        console.log(data);
+        res.end(data);
+    });
+    console.log("Stored user " + currentUser.primaryKey)
+    if (logoutTimerId!=null) {
+        clearInterval(logoutTimerId)
+    }
+    logoutTimerId = setTimeout(function() {
+        currentUser.primaryKey = null;
+        console.log("Logged out after one minuite " + currentUser.primaryKey)
+    }, 5000);
 });
 
-app.get('/getLoginData', function (req, res) {
-  console.log("Got read request")
+app.get('/getLoginData', function(req, res) {
+    console.log("Got read request")
     res.setHeader('Content-Type', 'application/json');
     res.send(res.jsonp(currentUser));
 });
-var server = app.listen(8081, function () {
+var server = app.listen(8081, function() {
 
-  var host = server.address().address
-  var port = server.address().port
-  console.log("Multi User app listening at http://%s:%s (TEST VERSION 1.1)", host, port)
-  console.log("Reading data....")
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Multi User app listening at http://%s:%s (TEST VERSION 1.1)", host, port)
+    console.log("Reading data....")
 
 })
